b1b8abbec41d3a47c510ecef796cb26f0e629b09
stanbankras <stanbankras@gmail.com>
2022-03-13 11:27:21 +0100

    Add basic data to backtest result, next to chart

--- frontend/src/components/strategies/BacktestResult.vue
+++ frontend/src/components/strategies/BacktestResult.vue
@@ -1,7 +1,16 @@
 <template>
   <div class="backtest-result">
-    Backtest result - from: {{ backtest.startBalance }} to: {{ backtest.endBalance }}
-    <line-chart v-bind="lineChartProps"/>
+    <div class="chart">
+      <line-chart v-bind="lineChartProps"/>
+    </div>
+    <div class="data">
+      <h3>Strategy name: {{ backtest.strategy.name }}</h3>
+      <p
+        class="result"
+      >Result <span :class="{ negative: backtestData.change < 0 }">{{ backtestData.change }}%</span></p>
+      <p>{{ backtestData.winsLosses.wins }} wins / {{ backtestData.winsLosses.losses }} losses (win rate: {{ Number((backtestData.winsLosses.winRate).toFixed(2)) }})</p>
+      <p class="backtest-date">Backtested on {{ moment(backtest.startedOn).format('DD MMM HH:mm') }}</p>
+    </div>
   </div>
 </template>
 
@@ -11,6 +20,9 @@ import { computed, defineComponent, PropType } from 'vue'
 import { LineChart, useLineChart } from 'vue-chart-3'
 import { Chart, ChartData, registerables } from 'chart.js'
 import 'chartjs-adapter-moment'
+import moment from 'moment'
+
+import BacktestPosition from '@/handlers/BacktestPosition'
 
 interface LineChartEntry {
   y: number
@@ -27,17 +39,41 @@ export default defineComponent({
     }
   },
   setup (props) {
+    const backtestData = computed(() => {
+      return {
+        change: Number(((props.backtest.endBalance - props.backtest.startBalance) / props.backtest.startBalance * 100).toFixed(2)),
+        fromDate: new Date(props.backtest.fromDate),
+        toDate: new Date(props.backtest.toDate),
+        winsLosses: calculateWinRate(props.backtest.positions)
+      }
+    })
+
+    function calculateWinRate (positions: Position[]): { winRate: number, wins: number, losses: number } {
+      if (!positions) return { winRate: 0, wins: 0, losses: 0 }
+
+      let wins = 0
+      let losses = 0
+
+      for (const position of positions) {
+        const pos = new BacktestPosition(position)
+        if (pos.getChangePercent() < 0) {
+          wins += 1
+        } else {
+          losses += 1
+        }
+      }
+
+      return { winRate: wins / losses, wins, losses }
+    }
+
     function calculateBalances (startBalance: number, positions: Position[]): LineChartEntry[] {
       if (!positions) return []
 
       let balance = startBalance
       const entries: LineChartEntry[] = []
       for (const position of positions) {
-        const quoteEntrySize = position.entryValue.rate * position.entryValue.baseSize
-        const quoteExitSize = position.exitValue.rate * position.exitValue.baseSize
-        const difference = quoteExitSize - quoteEntrySize
-
-        balance = balance + difference - position.entryValue.quoteFees - position.exitValue.quoteFees
+        const pos = new BacktestPosition(position)
+        balance = balance + pos.getQuoteDifference()
 
         if (new Date(position.closedAt).getTime() < 37701695) continue
 
@@ -50,14 +86,22 @@ export default defineComponent({
       return entries
     }
 
-    const dataValues = computed(() => calculateBalances(props.backtest.startBalance, props.backtest.positions))
+    const dataValues = computed(() => {
+      const chartValues = calculateBalances(props.backtest.startBalance, props.backtest.positions)
+
+      return {
+        chartValues
+      }
+    })
 
     const chartData = computed<ChartData<'line'>>(() => ({
       datasets: [
         {
-          data: dataValues.value as any,
+          data: dataValues.value.chartValues as any,
           label: 'Balance',
-          borderColor: '#3781d6'
+          borderColor: '#3781d6',
+          pointBackgroundColor: 'transparent',
+          pointBorderColor: 'transparent'
         }
       ]
     }))
@@ -67,7 +111,7 @@ export default defineComponent({
       options: {
         elements: {
           point: {
-            radius: 0
+            radius: 8
           }
         },
         scales: {
@@ -84,8 +128,42 @@ export default defineComponent({
 
     return {
       lineChartProps,
-      lineChartRef
+      lineChartRef,
+      backtestData,
+      moment
     }
   }
 })
 </script>
+
+<style lang="scss" scoped>
+.backtest-result {
+  display: grid;
+  grid-template-columns: minmax(900px, 2fr) 1fr;
+  gap: 2rem;
+  padding: 2rem 0;
+  margin: 1rem 0;
+  border-top: 1px solid var(--border-color);
+  p {
+    margin-bottom: 0.5rem;
+  }
+  .result {
+    span {
+      color: green;
+      font-weight: bold;
+      font-size: 20px;
+      margin-left: 1rem;
+      &.negative {
+        color: red;
+      }
+    }
+  }
+  .backtest-date {
+    margin-top: 1rem;
+    color: var(--text-tertiary);
+  }
+}
+.chart {
+  width: 100%;
+}
+</style>
---
+++ frontend/src/handlers/BacktestPosition.ts
@@ -0,0 +1,23 @@
+import { Position } from '@/types/Backtest'
+
+export default class BacktestPosition {
+  public position: Position
+
+  constructor (position: Position) {
+    this.position = position
+  }
+
+  getChangePercent (): number {
+    return (this.position.exitValue.rate - this.position.entryValue.rate) / this.position.entryValue.rate * 100
+  }
+
+  getQuoteDifference (): number {
+    const quoteEntrySize = this.position.entryValue.rate * this.position.entryValue.baseSize
+    const quoteExitSize = this.position.exitValue.rate * this.position.exitValue.baseSize
+    return quoteExitSize - quoteEntrySize - this.getFees()
+  }
+
+  getFees (): number {
+    return this.position.entryValue.quoteFees + this.position.exitValue.quoteFees
+  }
+}
